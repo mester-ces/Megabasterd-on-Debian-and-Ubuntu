d5928db68f2330ac3d33628c403082001598fdc2
mester-ces <52388120+mester-ces@users.noreply.github.com>
2020-03-21 17:56:51 +0100

    Add files via upload

---
+++ MegaBasterd Ubuntu and Debian Install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt install wget
+
+cd /opt/
+wget https://github.com/tonikelope/megabasterd/releases/download/v7.23/MegaBasterd_7.23.jar
+
+
+ln /opt/MegaBasterd_7.23 /usr/share/applications
